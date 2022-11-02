d16d6f7524d5173099706959e34f0ca1c6a0e943
unknown <gksrkfkaryghl@gmail.com>
2022-11-02 21:26:29 +0900

    reactQuery Practice

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -181,7 +181,7 @@ function Coin() {
       ["tickers", coinId],
       () => fetchCoinTickers(coinId!),
       {
-        refetchInterval: 5000,
+        // refetchInterval: 5000,
       }
     );
     const loading = infoLoading || tickersLoading;
--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,53 @@
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { fetchCoinTickers } from "../api";
+
+
+interface PriceData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+      USD: {
+        ath_date: string;
+        ath_price: number;
+        market_cap: number;
+        market_cap_change_24h: number;
+        percent_change_1h: number;
+        percent_change_1y: number;
+        percent_change_6h: number;
+        percent_change_7d: number;
+        percent_change_12h: number;
+        percent_change_15m: number;
+        percent_change_24h: number;
+        percent_change_30d: number;
+        percent_change_30m: number;
+        percent_from_price_ath: number;
+        price: number;
+        volume_24h: number;
+        volume_24h_change_24h: number;
+      };
+    };
+  }
+
+
 function Price() {
-    return <h1>Price</h1>;
+    
+    const {coinId} = useParams();
+
+    const { isLoading, data } = useQuery<PriceData>(
+        ["Price", coinId],
+        () => fetchCoinTickers(coinId!),
+      );
+
+    return <p>{data?.quotes.USD.percent_change_30m}</p>;
 }
   
 export default Price;
