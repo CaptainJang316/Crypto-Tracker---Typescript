116117a863ab929ec0727ae70c03ef2b8236c118
unknown <gksrkfkaryghl@gmail.com>
2022-11-03 20:49:55 +0900

    Update Price Component

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { fetchCoinTickers } from "../api";
-
+import { AiFillCaretUp, AiFillCaretDown } from "react-icons/ai";
 
 interface PriceData {
     id: string;
@@ -38,16 +39,92 @@ interface PriceData {
   }
 
 
+  const PriceDiv = styled.div`
+        text-align: center;
+        padding-bottom: 50px;
+  `;
+
+  const CurrentPrice = styled.h1`
+        padding: 20px;
+        padding-bottom: 30px;
+        font-size: 20px;
+  `;
+
+  const PriceChange = styled.span`
+        padding: 15px;
+        font-size: 17px;
+        color: ${(props) => props.color}
+  `;
+
+  const RedAiFillCaretUp = styled(AiFillCaretUp)`
+    color: red;
+  `;
+
+  const BlueAiFillCaretDown = styled(AiFillCaretDown)`
+    color: blue;
+  `;
+
+
 function Price() {
+
+    
     
     const {coinId} = useParams();
 
     const { isLoading, data } = useQuery<PriceData>(
         ["Price", coinId],
-        () => fetchCoinTickers(coinId!),
+        () => fetchCoinTickers(coinId!), {
+            refetchInterval: 1000,
+        }
       );
-
-    return <p>{data?.quotes.USD.percent_change_30m}</p>;
+    
+    return (
+        <PriceDiv>
+          {isLoading ? ("isLoading..")
+            : (
+              <>
+                <CurrentPrice>$ {data?.quotes.USD.price.toFixed(3)}</CurrentPrice>
+                  {
+                    data?.quotes.USD.percent_change_1h && 0 < data?.quotes.USD.percent_change_1h ?
+                    (
+                      <PriceChange color="red">
+                        1h % : <AiFillCaretUp/> {data?.quotes.USD.percent_change_1h}
+                      </PriceChange>
+                    ) : (
+                      <PriceChange color="dodgerblue">
+                        1h % : <AiFillCaretDown/> {data?.quotes.USD.percent_change_1h}
+                      </PriceChange>
+                    )
+                  }
+                  {
+                    data?.quotes.USD.percent_change_24h && 0 < data?.quotes.USD.percent_change_24h ?
+                    (
+                      <PriceChange color="red">
+                        24h % : <AiFillCaretUp/> {data?.quotes.USD.percent_change_24h}
+                      </PriceChange>
+                    ) : (
+                      <PriceChange color="dodgerblue">
+                        24h % : <AiFillCaretDown/> {data?.quotes.USD.percent_change_24h}
+                      </PriceChange>
+                    )
+                  }
+                  {
+                    data?.quotes.USD.percent_change_7d && 0 < data?.quotes.USD.percent_change_7d ?
+                    (
+                      <PriceChange color="red">
+                        7d % : <AiFillCaretUp/> {data?.quotes.USD.percent_change_7d}
+                      </PriceChange>
+                    ) : (
+                      <PriceChange color="dodgerblue">
+                        7d % : <AiFillCaretDown/> {data?.quotes.USD.percent_change_7d}
+                      </PriceChange>
+                    )
+                  }
+              </>
+            )
+        }
+        </PriceDiv>
+    );
 }
   
 export default Price;
